1ba9729b1a71d3cd1e5f2f567659bedaa3e6854d
Lawrence Lim <ll4715@nyu.edu>
2022-08-13 18:27:43 +0800

    deleted somehting

--- hardhat-simple-storage-fcc/tasks/block-number.ts
+++
@@ -1,8 +0,0 @@
-import { task } from "hardhat/config"
-
-export default task("block-number", "Get the current block number").setAction(
-  async (taskArgs, hre) => {
-    const blockNumber = await hre.ethers.provider.getBlockNumber()
-    console.log(`Current block number: ${blockNumber}`)
-  }
-)
